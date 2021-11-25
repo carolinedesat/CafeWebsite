c5ceb64ba92c49a657ae119d71041b0fc08746e6
Caroline de Sá Teixeira <caroline.desat@gmail.com>
2021-11-25 17:10:00 +0000

    js

---
+++ index.js
@@ -0,0 +1,7 @@
+const   http = require('http'), //this module provides the HTTP server functionalities
+        path = requite('path'), //this path module provides utilities for working with file and directory paths
+        express = require('express'), //this module allows this app to respond to HTTP requests, defines the routing and renders back the required content
+        fs = require('fs'), //this module allows to work with the file system: read and write files back
+        xmlParse = require('xslt-processor').smlParse, //this module allows to work with XML files
+        xsltParse = require('xslt-processor').xsltProcess, //the same module allows us to utilise XSL Transformations
+        xml2js = require('xml2js') //this module does XML <-> JSON conversion
