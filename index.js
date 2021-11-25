ab2cda6960c17fcae3f73d39c45d3b665ad872cf
Caroline de Sá Teixeira <caroline.desat@gmail.com>
2021-11-25 17:25:06 +0000

    jsv3

--- index.js
+++ index.js
@@ -4,4 +4,29 @@ const   http = require('http'), //this module provides the HTTP server functiona
         fs = require('fs'), //this module allows to work with the file system: read and write files back
         xmlParse = require('xslt-processor').xmlParse, //this module allows to work with XML files
         xsltParse = require('xslt-processor').xsltProcess, //the same module allows us to utilise XSL Transformations
-        xml2js = require('xml2js') //this module does XML <-> JSON conversion
+        xml2js = require('xml2js'); //this module does XML <-> JSON conversion
+
+const   router = express();
+        server = http.createServer(router);
+
+router.get('/', function(req, res) {
+
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+
+    let xml = fs.readFileSync('PaddysCafe.xml', 'utf8');
+        xsl = fs.readFileSync('PaddysCafe.xsl', 'utf8');
+
+    let doc = xmlParse(xml),
+        stylesheet = xmlParse(xsl);
+
+    let result = xsltProcess(doc, stylesheet);
+
+    res.end(result.toString());
+
+});
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function()
+{
+    const addr = server.address();
+    console.log("Server listening at", addr.address + ":" + addr.port)
+});
